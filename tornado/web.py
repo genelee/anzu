f73e3322fa4b9d8cdc88e9efe2b6cb3fd372abf7
W-Mark Kubacki <wmark@hurrikane.de>
2010-02-01 16:55:08 +0100

    added Copyright notice as required by local jurisdiction

--- tornado/web.py
+++ tornado/web.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
-# Copyright 2009 Facebook
+# Copyright 2009-2010 Facebook
+# Copyright 2010 W-Mark Kubacki
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
