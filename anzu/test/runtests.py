d2ee0ca1e4473dcdfbb1b2206e3486ff4916b0ad
W-Mark Kubacki <wmark@hurrikane.de>
2011-10-20 20:09:56 +0200

    (1/4) file and folder renames

--- anzu/test/runtests.py
+++ anzu/test/runtests.py
@@ -2,20 +2,32 @@
 import unittest
 
 TEST_MODULES = [
-    'anzu.httputil.doctests',
-    'anzu.test.escape_test',
-    'anzu.test.httpserver_test',
-    'anzu.test.ioloop_test',
-    'anzu.test.iostream_test',
-    'anzu.test.simple_httpclient_test',
-    'anzu.test.stack_context_test',
-    'anzu.test.testing_test',
-    'anzu.test.web_test',
+    'tornado.httputil.doctests',
+    'tornado.iostream.doctests',
+    'tornado.util.doctests',
+    'tornado.test.auth_test',
+    'tornado.test.curl_httpclient_test',
+    'tornado.test.escape_test',
+    'tornado.test.gen_test',
+    'tornado.test.httpclient_test',
+    'tornado.test.httpserver_test',
+    'tornado.test.httputil_test',
+    'tornado.test.import_test',
+    'tornado.test.ioloop_test',
+    'tornado.test.iostream_test',
+    'tornado.test.process_test',
+    'tornado.test.simple_httpclient_test',
+    'tornado.test.stack_context_test',
+    'tornado.test.template_test',
+    'tornado.test.testing_test',
+    'tornado.test.twisted_test',
+    'tornado.test.web_test',
+    'tornado.test.wsgi_test',
 ]
 
 def all():
     return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
 
 if __name__ == '__main__':
-    import anzu.testing
-    anzu.testing.main()
+    import tornado.testing
+    tornado.testing.main()
--- anzu/util.py
+++ anzu/util.py
@@ -1,69 +1,47 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright 2009-2010 W-Mark Kubacki; wmark@hurrikane.de
-#
+"""Miscellaneous utility functions."""
 
-__all__ = ['baseN', 'import_object', 'doctests']
+class ObjectDict(dict):
+    """Makes a dictionary behave like an object."""
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
 
 def import_object(name):
     """Imports an object by name.
 
     import_object('x.y.z') is equivalent to 'from x.y import z'.
 
-    >>> import anzu.escape
-    >>> import_object('anzu.escape') is anzu.escape
+    >>> import tornado.escape
+    >>> import_object('tornado.escape') is tornado.escape
     True
-    >>> import_object('anzu.escape.utf8') is anzu.escape.utf8
+    >>> import_object('tornado.escape.utf8') is tornado.escape.utf8
     True
     """
     parts = name.split('.')
     obj = __import__('.'.join(parts[:-1]), None, None, [parts[-1]], 0)
     return getattr(obj, parts[-1])
 
+# Fake byte literal support:  In python 2.6+, you can say b"foo" to get
+# a byte literal (str in 2.x, bytes in 3.x).  There's no way to do this
+# in a way that supports 2.5, though, so we need a function wrapper
+# to convert our string literals.  b() should only be applied to literal
+# latin1 strings.  Once we drop support for 2.5, we can remove this function
+# and just use byte literals.
+if str is unicode:
+    def b(s):
+        return s.encode('latin1')
+    bytes_type = bytes
+else:
+    def b(s):
+        return s
+    bytes_type = str
+
 def doctests():
     import doctest
     return doctest.DocTestSuite()
-
-base_representation_chars = "0123456789abcdefghijklmnopqrstuvwxyz"
-
-def baseN(num, b):
-    """Converts the unsigned integer 'num' into its string representation
-    in positional numeral system with base 'b'.
-
-    Use this to get a short string representation of an otherwise long
-    integer, e.g. hashes.
-
-    Examples::
-
-        >>> baseN(300, 36)
-        '8c'
-        >>> baseN(300, 10)
-        '300'
-        >>> baseN(300, 16)
-        '12c'
-        >>> baseN(300, 8)
-        '454'
-        >>> baseN(300, 5)
-        '2200'
-        >>> baseN(300, 2)
-        '100101100'
-        >>> baseN(0, 14)
-        '0'
-        >>> n = 2189753199
-        >>> baseN(n, 36)
-        '107q0dr'
-        >>> n
-        2189753199
-    """
-    assert num >= 0, "'num' must be positive"
-    assert 0 < b <= len(base_representation_chars), \
-        "radix 'b' must be greater than 0 and less or equal than 36"
-    if num == 0:
-        return "0"
-    else:
-        r = ''
-        while num > 0:
-            r += base_representation_chars[num % b]
-            num = num // b
-        return r[::-1]
