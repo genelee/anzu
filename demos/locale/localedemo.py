e15e3568beeb6bbc1b2b41f688fb2dfb50422ad1
W-Mark Kubacki <wmark@hurrikane.de>
2011-10-20 22:09:56 +0200

    More comments in the l10n `locale` demo.

--- demos/locale/localedemo.py
+++ demos/locale/localedemo.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 W-Mark Kubacki; wmark@hurrikane.de
+# Copyright 2010-2011 W-Mark Kubacki; wmark@hurrikane.de
 #
 
-import os.path
 import anzu.httpserver
 import anzu.ioloop
+import anzu.options
 import anzu.web
+
+import os.path
 import anzu.locale
+
+from anzu.options import define, options, enable_pretty_logging
+
+define("port", default=8888, help="run on the given port", type=int)
+
 
 @anzu.web.location('/')
 class MainHandler(anzu.web.RequestHandler):
@@ -19,10 +26,20 @@ class MainHandler(anzu.web.RequestHandler):
             % (self.locale.code, anzu.locale.get_supported_locales(MainHandler)))
 
 
-if __name__ == "__main__":
+def main():
+    enable_pretty_logging()
+
+    # These two lines are the difference. By them translations are loaded.
     cwd = os.path.dirname(__file__)
     anzu.locale.load_gettext_translations(os.path.join(cwd, "locales"), "messages")
+
+    # here is nothing special
+    anzu.options.parse_command_line()
     application = anzu.web.Application()
     http_server = anzu.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(options.port)
     anzu.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
